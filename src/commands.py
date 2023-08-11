0a0c0b9aeac176865dad2fa9ad2bbfaff4540786
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-11 22:36:51 +0000

    Accessing the Digitalocean Embed through the getter instead of the creation method

--- src/commands.py
+++ src/commands.py
@@ -4,11 +4,12 @@ from src.ocean_embed import DigitalOceanEmbed
 class DiscordCommands(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self._bot = bot
-        self._embed = DigitalOceanEmbed()
+        self._ocean_embed= DigitalOceanEmbed()
   
     @commands.slash_command(name="digitalocean",description=  "Info of your digitalocean components")
     async def vps(self,interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
-       embed = await self._embed.embed_creation(option)
+       await self._ocean_embed.creation(option)
+       embed = self._ocean_embed.embed
        await interaction.response.send_message(embed=embed)
      
     @commands.slash_command(name = "latency",description="The bot´s latency")    
