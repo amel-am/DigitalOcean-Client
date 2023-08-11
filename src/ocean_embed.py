17c865983c0365a1be2803aaa170ac0f2722c366
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-11 22:33:46 +0000

    The embed variable was made into a private

--- src/ocean_embed.py
+++ src/ocean_embed.py
@@ -6,21 +6,25 @@ from src.ocean_client import DigitalOceanClient
 class DigitalOceanEmbed():
     def __init__(self):
         self._ocean_client = DigitalOceanClient()
-        self.embed = disnake.Embed(title="",description="",color=disnake.Color.blue())
-        self.embed.set_image("https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
+        self._embed = disnake.Embed(title="",description="",color=disnake.Color.blue())
+        self._embed.set_image("https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
     
-    async def _cat(self,k: str,v: str,n: int = 1,marks: str = None):
+    @property
+    def embed(self):
+        return self._embed
+    
+    async def _cat(self,k: str,v: str,n: int = 1,marks: str = None) -> None:
         lines = "\n" * n
         if marks != None:
-            self.embed.description += f"{k}:{marks}{v}{marks}{lines}" 
+            self._embed.description += f"{k}:{marks}{v}{marks}{lines}" 
         else:
-            self.embed.description += f"{k}:{v}{lines}" 
+            self._embed.description += f"{k}:{v}{lines}" 
             
-    async def _acc_emb(self):
+    async def _acc_emb(self) -> None:
         account_info = await self._ocean_client.get_account()
         await self._dict_loop(account_info)
             
-    async def _dict_loop(self,dict_:dict):
+    async def _dict_loop(self,dict_:dict) -> None:
         for k,v in dict_.items():
             if isinstance(v,(list,dict)):
                 continue
@@ -34,21 +38,21 @@ class DigitalOceanEmbed():
                 case _:
                     await self._cat(k,v)
                     
-    async def _list_loop(self,list_:list):
+    async def _list_loop(self,list_:list) -> None:
         for e in list_:
             await self._dict_loop(e)
                      
-    async def _droplets_emb(self):
+    async def _droplets_emb(self) -> None:
         droplets_info = await self._ocean_client.get_droplets()
         await self._list_loop(droplets_info)
     
-    async def _ssh_emb(self):
+    async def _ssh_emb(self) -> None:
         ssh_info = await self._ocean_client.get_keys()
         await self._list_loop(ssh_info)
                     
-    async def embed_creation(self,option:str):
-        self.embed.title = f"{option} info"
-        self.embed.description = ""
+    async def creation(self,option:str) -> None: 
+        self._embed.title = f"{option} info"
+        self._embed.description = ""
         match option:
             case "ssh":
                 await self._ssh_emb()
@@ -56,7 +60,7 @@ class DigitalOceanEmbed():
                 await self._acc_emb()
             case "droplets":
                 await self._droplets_emb()
-        self.embed.remove_footer()
-        self.embed.set_footer(text=self._ocean_client._ratelimit)
-        return self.embed
+        self._embed.remove_footer()
+        self._embed.set_footer(text=self._ocean_client.ratelimit)
+
                 
