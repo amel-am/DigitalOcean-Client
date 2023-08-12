5a4cc162dbabcd77f9111d016e176477d627eba2
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-12 02:46:56 +0000

    Changed the name arg from requests to commands

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from disnake.ext import commands
 from src.yaml import slash_guilds, discord_token
 from src.commands import DiscordCommands
 
-bot = commands.InteractionBot(test_guilds=slash_guilds,activity=disnake.Activity(type=disnake.ActivityType.listening,name="requests"))
+bot = commands.InteractionBot(test_guilds=slash_guilds,activity=disnake.Activity(type=disnake.ActivityType.listening,name="commands"))
                
 if __name__ == "__main__":      
   bot.add_cog(DiscordCommands(bot))
