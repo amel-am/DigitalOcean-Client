db2e1dc43627f0c236bccc4c0d26031ff73ab59a
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-12 02:44:31 +0000

    Renamed vps function  to ocean_info

--- src/commands.py
+++ src/commands.py
@@ -6,12 +6,16 @@ class DiscordCommands(commands.Cog):
         self._bot = bot
   
     @commands.slash_command(name="digitalocean",description=  "Info of your digitalocean components")
-    async def vps(self,interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
-       ocean_embed = DigitalOceanEmbed(option)
-       await ocean_embed.creation()
-       await interaction.response.send_message(embed=ocean_embed.get_embed)
+    async def ocean_info(self,interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
+        ocean_embed = DigitalOceanEmbed(option)
+        await ocean_embed.creation()
+        await interaction.response.send_message(embed=ocean_embed.get_embed)
      
     @commands.slash_command(name = "latency",description="The bot´s latency")    
     async def latency(self,interaction):
-      bot_latency = round(self._bot.latency * 1000,2)
-      await interaction.response.send_message(str(bot_latency) + "ms")
+        bot_latency = round(self._bot.latency * 1000,2)
+        await interaction.response.send_message(str(bot_latency) + "ms")
+      
+
+        
+        
