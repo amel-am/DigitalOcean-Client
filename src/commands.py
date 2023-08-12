8ad61fee66ba0aefa6bd37cfe2e1ddc6e5e80518
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-12 01:44:07 +0000

    Create a new instance of DigitalOceanEmbed class each time the slash command is used

--- src/commands.py
+++ src/commands.py
@@ -4,13 +4,12 @@ from src.ocean_embed import DigitalOceanEmbed
 class DiscordCommands(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self._bot = bot
-        self._ocean_embed= DigitalOceanEmbed()
   
     @commands.slash_command(name="digitalocean",description=  "Info of your digitalocean components")
     async def vps(self,interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
-       await self._ocean_embed.creation(option)
-       embed = self._ocean_embed.embed
-       await interaction.response.send_message(embed=embed)
+       ocean_embed = DigitalOceanEmbed(option)
+       await ocean_embed.creation()
+       await interaction.response.send_message(embed=ocean_embed.get_embed)
      
     @commands.slash_command(name = "latency",description="The bot´s latency")    
     async def latency(self,interaction):
