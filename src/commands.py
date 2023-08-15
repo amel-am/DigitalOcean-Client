98b3c01cf782ac4e217d4b50d15d75454137ce10
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-15 03:39:37 +0000

    Added a new slash command

--- src/commands.py
+++ src/commands.py
@@ -5,8 +5,8 @@ class DiscordCommands(commands.Cog):
     def __init__(self,bot:commands.Bot):
         self._bot = bot
   
-    @commands.slash_command(name="digitalocean",description=  "Info of your digitalocean components")
-    async def ocean_info(self,interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
+    @commands.slash_command(name="digitalocean",description= "Info of your digitalocean components")
+    async def ocean_info(interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
         ocean_embed = DigitalOceanEmbed(option)
         await ocean_embed.creation()
         await interaction.response.send_message(embed=ocean_embed.get_embed)
@@ -15,6 +15,10 @@ class DiscordCommands(commands.Cog):
     async def latency(self,interaction):
         bot_latency = round(self._bot.latency * 1000,2)
         await interaction.response.send_message(str(bot_latency) + "ms")
+        
+    @commands.slash_command(name="activity", description="The activity of your bot")
+    async def activity(self,interaction,option=commands.Param(autocomplete=[""])):
+        dict_object = {}
       
 
         
