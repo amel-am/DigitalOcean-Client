251c029bf3f9a769e251fd82061bc64b42fb4d8e
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-16 22:35:29 +0200

    Following pep 8 guidelines

--- src/commands.py
+++ src/commands.py
@@ -1,25 +1,19 @@
+from disnake import ApplicationCommandInteraction
 from disnake.ext import commands
 from src.ocean_embed import DigitalOceanEmbed
 
+
 class DiscordCommands(commands.Cog):
-    def __init__(self,bot:commands.Bot):
+    def __init__(self, bot: commands.Bot):
         self._bot = bot
-  
-    @commands.slash_command(name="digitalocean",description= "Info of your digitalocean components")
-    async def ocean_info(interaction,option = commands.Param(autocomplete=["ssh","droplets","account"])):
+
+    @commands.slash_command(dm_permission=True, name="digitalocean", description="Info of your digitalocean components")
+    async def ocean_info(self, interaction: ApplicationCommandInteraction, option: str = commands.Param(choices=["ssh", "droplets", "account"])):
         ocean_embed = DigitalOceanEmbed(option)
         await ocean_embed.creation()
         await interaction.response.send_message(embed=ocean_embed.get_embed)
-     
-    @commands.slash_command(name = "latency",description="The bot´s latency")    
-    async def latency(self,interaction):
-        bot_latency = round(self._bot.latency * 1000,2)
-        await interaction.response.send_message(str(bot_latency) + "ms")
-        
-    @commands.slash_command(name="activity", description="The activity of your bot")
-    async def activity(self,interaction,option=commands.Param(autocomplete=[""])):
-        dict_object = {}
-      
 
-        
-        
+    @commands.slash_command(name="latency", description="The bot's latency")
+    async def latency(self, interaction: ApplicationCommandInteraction):
+        bot_latency = round(self._bot.latency * 1000, 2)
+        await interaction.response.send_message(str(bot_latency) + "ms")
