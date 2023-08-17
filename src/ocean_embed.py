8fdae78df7613173c37af833876d7e615d43f660
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-17 20:23:36 +0200

    Removed async from functions that dont have any I/O operations. Removed unused imported module as well

--- src/ocean_embed.py
+++ src/ocean_embed.py
@@ -1,60 +1,63 @@
 import disnake
-import datetime
 from src.ocean_client import DigitalOceanClient
 
-#This class is used to make the digitalocean embed
+# This class is used to make the digitalocean embed
+
+
 class DigitalOceanEmbed():
-    def __init__(self,option:str):
+    def __init__(self, option: str):
         self._option = option
         self._ocean_client = DigitalOceanClient()
-        self._embed = disnake.Embed(title=f"{option} info",description="",color=disnake.Color.blue())
-        self._embed.set_image("https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
-    
+        self._embed = disnake.Embed(
+            title=f"{option} info", description="", color=disnake.Color.blue())
+        self._embed.set_image(
+            "https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
+
     @property
     def get_embed(self):
         return self._embed
-    
-    async def _cat(self,k: str,v: str,n: int = 1,marks: str = None) -> None:
+
+    def _cat(self, k: str, v: str, n: int = 1, marks: str = None) -> None:
         lines = "\n" * n
         if marks != None:
-            self._embed.description += f"{k}:{marks}{v}{marks}{lines}" 
+            self._embed.description += f"{k}:{marks}{v}{marks}{lines}"
         else:
-            self._embed.description += f"{k}:{v}{lines}" 
-            
+            self._embed.description += f"{k}:{v}{lines}"
+
     async def _acc_emb(self) -> None:
         account_info = await self._ocean_client.get_account()
         await self._dict_loop(account_info)
-            
-    async def _dict_loop(self,dict_:dict) -> None:
-        for k,v in dict_.items():
-            if isinstance(v,(list,dict)):
+
+    def _dict_loop(self, dict_: dict) -> None:
+        for k, v in dict_.items():
+            if isinstance(v, (list, dict)):
                 continue
             match k:
                 case "fingerprint":
-                   await self._cat(k,v,1,"```")
+                    self._cat(k, v, 1, "```")
                 case  "vpc_uuid":
-                    await self._cat(k,v,2)
+                    self._cat(k, v, 2)
                 case "email":
                     continue
                 case _:
                     if v == "":
-                        await self._cat(k,"None")
+                        self._cat(k, "None")
                     else:
-                        await self._cat(k,v)
-                    
-    async def _list_loop(self,list_:list) -> None:
+                        self._cat(k, v)
+
+    def _list_loop(self, list_: list) -> None:
         for e in list_:
-            await self._dict_loop(e)
-                     
+            self._dict_loop(e)
+
     async def _droplets_emb(self) -> None:
         droplets_info = await self._ocean_client.get_droplets()
         await self._list_loop(droplets_info)
-    
+
     async def _ssh_emb(self) -> None:
         ssh_info = await self._ocean_client.get_keys()
         await self._list_loop(ssh_info)
-                    
-    async def creation(self) -> None: 
+
+    async def creation(self) -> None:
         match self._option:
             case "ssh":
                 await self._ssh_emb()
