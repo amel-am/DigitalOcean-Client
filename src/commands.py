24ecfd518012e532ee53d2aba4d84e035aeb5206
amel-am <96318827+amel-am@users.noreply.github.com>
2023-08-28 00:08:16 +0330

    Added a description for the option parameter and made a new command for changing activity of bot.

--- src/commands.py
+++ src/commands.py
@@ -1,4 +1,4 @@
-from disnake import ApplicationCommandInteraction, ActivityType
+from disnake import ApplicationCommandInteraction, ActivityType, Status, Activity
 from disnake.ext import commands
 from src.ocean_embed import DigitalOceanEmbed
 
@@ -8,7 +8,7 @@ class DiscordCommands(commands.Cog):
         self._bot = bot
 
     @commands.slash_command(name="digitalocean", description="Info of your digitalocean components")
-    async def ocean_info(self, interaction: ApplicationCommandInteraction, option: str = commands.Param(choices=["ssh", "droplets", "account"])):
+    async def ocean_info(self, interaction: ApplicationCommandInteraction, option: str = commands.Param(choices=["ssh", "droplets", "account"], description="Choose what to retrieve.")):
         ocean_embed = DigitalOceanEmbed(option)
         await ocean_embed.creation()
         await interaction.response.send_message(embed=ocean_embed.get_embed)
@@ -17,3 +17,8 @@ class DiscordCommands(commands.Cog):
     async def latency(self, interaction: ApplicationCommandInteraction):
         bot_latency = round(self._bot.latency * 1000, 2)
         await interaction.response.send_message(str(bot_latency) + "ms")
+
+    @commands.slash_command(name="activity", description="Change the bot's activity")
+    async def activity(self, interaction: ApplicationCommandInteraction, activitytype: ActivityType, activity: str):
+        await self._bot.change_presence(activity=Activity(name=activity, type=activitytype))
+        await interaction.response.send_message(f"The bot's activity was changed to {ActivityType(activitytype).name} {activity}")
