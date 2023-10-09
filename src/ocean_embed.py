bc28ec0df3a2be60123fd96b7ddb1bc8a7ee6334
amel-am <96318827+amel-am@users.noreply.github.com>
2023-10-09 03:46:48 +0330

    Added a function for traversing deeply nested elements in either lists or dicts to then add them as embed fields

--- src/ocean_embed.py
+++ src/ocean_embed.py
@@ -1,61 +1,113 @@
 import disnake
-from src.ocean_client import DigitalOceanClient
-
+from typing import Union
+from .ocean_client import DigitalOceanClient
 # This class is used to make the digitalocean embed
 
 
-class DigitalOceanEmbed():
+class DiscordEmbedDropdown(disnake.ui.StringSelect):
     def __init__(self, option: str):
+        super().__init__(
+            placeholder="Choose an option",
+            min_values=1,
+            max_values=1,
+            row=1,
+        )
         self._option = option
         self._ocean_client = DigitalOceanClient()
-        self._embed = disnake.Embed(
-            title=f"{option} info", description="", color=disnake.Color.blue())
-        self._embed.set_image(
-            "https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
+        self._index = 0
+        self._embed = None
+        self._page_length = 1
 
     @property
     def get_embed(self):
         return self._embed
 
-    def _cat(self, k: str, v: str, n: int = 1, marks: str = None) -> None:
+    async def callback(self, inter: disnake.MessageInteraction):
+        self._index = int(self.values[0])
+        await self.creation()
+        await inter.response.edit_message(embed=self._embed)
+
+    def _itemloop(self, label=None, array: list = None):
+        for i in range(len(array)):
+            self.append_option(
+                disnake.SelectOption(label=f"{label} {i + 1}: {array[i]['name']}", value=str(i)))
+
+    def _add_description(self, k: str, v: str, n: int = 1, marks: str = None) -> None:
         lines = "\n" * n
-        if marks != None:
-            self._embed.add_field(k, v, inline=False)
+        if marks == None:
+            self._embed.description += f"{k}:{v}{lines}"
         else:
-            self._embed.add_field(k, v, inline=False)
+            self._embed.description += f"{k}:{marks}{v}{marks}{lines}"
+
+    def _reassign_embed(self, label: str) -> None:
+        self._embed = disnake.Embed(
+            title=f"{label} (Page {self._index + 1} of {self._page_length})", description="", color=disnake.Color.blue())
+        self._embed.set_image(
+            "https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png")
+
+    def _unpack(self, iterable: Union[list, dict] = None, condition: dict = None, value: str = ""):
+        if isinstance(iterable, list):
+            for e in iterable:
+                if isinstance(e, dict):
+                    value = self._unpack(e, condition, value)
+                else:
+                    value = ",".join(iterable)
+        else:
+            for k, v in iterable.items():
+                if isinstance(v, list):
+                    if v:
+                        value += f"***{k}***\n"
+                    value = self._unpack(v, condition, value)
+                else:
+                    if k in condition:
+                        value += f"{k}:{v}{condition[k]}"
+                    else:
+                        value += f"{k}:{v}\n"
+        return value
 
     async def _acc_emb(self) -> None:
         account_info = await self._ocean_client.get_account()
-        self._dict_loop(account_info)
-
-    def _dict_loop(self, dict_: dict) -> None:
-        for k, v in dict_.items():
-            if isinstance(v, (list, dict)):
+        self._reassign_embed("account info")
+        for k, v in account_info.items():
+            if k == "email":
                 continue
-            match k:
-                case "fingerprint":
-                    self._cat(k, v, 1, "```")
-                case  "vpc_uuid":
-                    self._cat(k, v)
-                case "email":
-                    continue
-                case _:
-                    if v == "":
-                        self._cat(k, "None")
-                    else:
-                        self._cat(k, v)
-
-    def _list_loop(self, list_: list) -> None:
-        for e in list_:
-            self._dict_loop(e)
+            elif k == "team":
+                self._embed.add_field(k, f"{v}\n")
+            else:
+                self._add_description(k, v)
 
     async def _droplets_emb(self) -> None:
         droplets_info = await self._ocean_client.get_droplets()
-        self._list_loop(droplets_info)
+        self._page_length = len(droplets_info)
+        self._reassign_embed("droplet info")
+        self._itemloop("droplet", droplets_info)
+        for k, v in droplets_info[self._index].items():
+            if k == "networks":
+                value = self._unpack(v, {"type": "\n\n"})
+                self._embed.add_field(
+                    name="networks", value=value, inline=False)
+            elif k == "features":
+                value = self._unpack(v, {})
+                self._embed.add_field(
+                    name="features", value=value, inline=False)
+            elif k == "image":
+                value = self._unpack(v, {})
+                self._embed.add_field(
+                    name="image", value=value, inline=False)
+            else:
+                if not isinstance(v, (list, dict)):
+                    self._add_description(k, v)
 
     async def _ssh_emb(self) -> None:
         ssh_info = await self._ocean_client.get_keys()
-        self._list_loop(ssh_info)
+        self._page_length = len(ssh_info)
+        self._reassign_embed("ssh info")
+        self._itemloop("ssh key", ssh_info)
+        for k, v in ssh_info[self._index].items():
+            if k == "fingerprint":
+                self._add_description(k, v, 1, "```")
+            else:
+                self._add_description(k, v)
 
     async def creation(self) -> None:
         match self._option:
@@ -63,8 +115,8 @@ class DigitalOceanEmbed():
                 await self._ssh_emb()
             case "account":
                 await self._acc_emb()
-            case "droplets":
+            case "droplet":
                 await self._droplets_emb()
-
-        self._embed.set_footer(text=self._ocean_client._ratelimit_result)
+        self._embed.set_footer(
+            text=self._ocean_client._ratelimit_result)
         self._embed.timestamp = self._ocean_client._ratelimit_time
