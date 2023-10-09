ab3d657b3f5fc194390d8871d23fa4932e0ea874
amel-am <96318827+amel-am@users.noreply.github.com>
2023-10-09 03:52:08 +0330

    Made a DropdownView class in order to add the Dropdown.

---
+++ src/dropdown_view.py
@@ -0,0 +1,15 @@
+import disnake
+from .ocean_embed import DiscordEmbedDropdown
+
+
+class DropdownView(disnake.ui.View):
+    def __init__(self, item: DiscordEmbedDropdown):
+        super().__init__(timeout=None)
+        self._item = item
+        if self._item.options:
+            self.add_item(self._item)
+
+    @disnake.ui.button(label="Refresh embed", emoji="🔃", custom_id="1", row=2)
+    async def refresh_button(self, button: disnake.ui.button, interaction: disnake.MessageInteraction):
+        await self._item.creation()
+        await interaction.response.edit_message(embed=self._item.get_embed)
