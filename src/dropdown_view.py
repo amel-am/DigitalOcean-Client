08b70442f5c76deda43c5af326666e5b40385884
amel-am <96318827+amel-am@users.noreply.github.com>
2023-10-13 21:11:45 +0330

    Fixed wrong method name.

--- src/dropdown_view.py
+++ src/dropdown_view.py
@@ -11,5 +11,5 @@ class DropDownView(disnake.ui.View):
 
     @disnake.ui.button(label="Refresh embed", emoji="🔃", custom_id="1", row=2)
     async def refresh_button(self, button: disnake.ui.button, interaction: disnake.MessageInteraction):
-        await self._item.creation()
-        await interaction.response.edit_message(embed=self._item.get_embed)
+        await self._item.create_embed()
+        await interaction.response.edit_message(embed=self._item.embed)
