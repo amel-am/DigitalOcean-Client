087aa04c991dd1fc7b7d6c83bc57e73ca8288907
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-03 07:17:28 +0330

    Not using add view in on ready event handler.

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from disnake.ext import commands
 from src.yaml import slash_guilds, discord_token
 from src.commands import DiscordCommands
 
-bot = commands.InteractionBot(test_guilds=slash_guilds,activity=disnake.Activity(type=disnake.ActivityType.listening,name="commands"))
-               
-if __name__ == "__main__":      
-  bot.add_cog(DiscordCommands(bot))
-  bot.run(discord_token)
+bot = commands.InteractionBot(test_guilds=slash_guilds, activity=disnake.Activity(
+    type=disnake.ActivityType.listening, name="commands"))
+
+
+@bot.event
+async def on_ready():
+    print("Digitalocean bot is ready!")
+
+
+if __name__ == "__main__":
+    bot.add_cog(DiscordCommands(bot))
+    bot.run(discord_token)
