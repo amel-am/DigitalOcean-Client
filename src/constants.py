cacd2dc80ae8fcd064f626b3fd9171094c3a2556
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-03 07:26:35 +0330

    Make a choices constant which has the list of all available choices for the command available.

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,11 @@
 from enum import StrEnum
 
+CHOICES = ["ssh", "droplet", "account"]
+
+
 class RequestMethods(StrEnum):
     GET = "GET"
     POST = "POST"
     PUT = "PUT"
     PATCH = "PATCH"
     DELETE = "DELETE"
-    
-        
