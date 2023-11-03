b43d0b513d7664b2f820db8c6c7c447aa6274531
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-03 07:41:04 +0330

    Fix bug where the embed would be updated with the wrong information when clicking the update contents button.

--- src/dropdown_view.py
+++ src/dropdown_view.py
@@ -1,15 +1,31 @@
 import disnake
 from .ocean_embed import DiscordEmbedDropdown
+from re import search
 
 
 class DropDownView(disnake.ui.View):
-    def __init__(self, item: DiscordEmbedDropdown):
+    def __init__(self, embed_dropdown: DiscordEmbedDropdown):
         super().__init__(timeout=None)
-        self._item = item
-        if self._item.options:
-            self.add_item(self._item)
+        self._embed_dropdown = embed_dropdown
+        if self._embed_dropdown.options:
+            self.add_item(self._embed_dropdown)
 
-    @disnake.ui.button(label="Refresh embed", emoji="🔃", custom_id="1", row=2)
-    async def refresh_button(self, button: disnake.ui.button, interaction: disnake.MessageInteraction):
-        await self._item.create_embed()
-        await interaction.response.edit_message(embed=self._item.embed)
+    @disnake.ui.button(label="Update embed contents", emoji="🔃", custom_id="1", row=2)
+    async def update_content(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
+        # Signal to user that the content is being updated.
+        await interaction.response.edit_message(content="The content is being updated...", embed=None)
+        # Get the original message to access the embed
+        message = interaction.message
+        embed = message.embeds[0]
+        # Extract the title from the embed
+        title = embed.title
+        # Find the start index of the first digit in the title
+        search_page = search("[0-9]", title).start()
+        # Extract the index by converting the digit to an integer and subtracting 1
+        self._embed_dropdown.index = int(title[search_page]) - 1
+        # Extract the choice part from the title
+        self._embed_dropdown.choice = title.split(" ")[0]
+        # Create a new embed based on the updated choice and index
+        await self._embed_dropdown.create_embed()
+        # Edit the original message with the new embed and updated view
+        await interaction.edit_original_message(content=None, embed=self._embed_dropdown.embed)
