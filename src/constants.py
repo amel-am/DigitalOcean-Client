6d8e1cabd6476f869eac019a1829ed9ddb707f3f
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-04 07:08:51 +0330

    Added new constants.

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,9 @@
 from enum import StrEnum
 
 CHOICES = ["ssh", "droplet", "account"]
+MAX_OPTIONS = 25
+PICTURE_URL = "https://doimages.nyc3.digitaloceanspaces.com/Droplet,Social,Blog,Email.png"
+BASE_API_URL = "https://api.digitalocean.com/v2"
 
 
 class RequestMethods(StrEnum):
