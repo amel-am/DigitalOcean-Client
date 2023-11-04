2e4e43707648fc23d5cbb9789839ca0c4f6d3a89
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-04 07:10:11 +0330

    Add + to the regex in case theres another digit, then use group to retrieve the match.

--- src/dropdown_view.py
+++ src/dropdown_view.py
@@ -14,18 +14,25 @@ class DropDownView(disnake.ui.View):
     async def update_content(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         # Signal to user that the content is being updated.
         await interaction.response.edit_message(content="The content is being updated...", embed=None)
+
         # Get the original message to access the embed
         message = interaction.message
         embed = message.embeds[0]
+
         # Extract the title from the embed
         title = embed.title
+
         # Find the start index of the first digit in the title
-        search_page = search("[0-9]", title).start()
-        # Extract the index by converting the digit to an integer and subtracting 1
-        self._embed_dropdown.index = int(title[search_page]) - 1
+        search_page = search("[0-9]+", title)
+
+        # Extract the page number
+        self._embed_dropdown.index = int(search_page.group()) - 1
+
         # Extract the choice part from the title
         self._embed_dropdown.choice = title.split(" ")[0]
+
         # Create a new embed based on the updated choice and index
         await self._embed_dropdown.create_embed()
+
         # Edit the original message with the new embed and updated view
         await interaction.edit_original_message(content=None, embed=self._embed_dropdown.embed)
