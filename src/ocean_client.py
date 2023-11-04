969af1aed329c1162b58bd7147c443b6f76f3f87
amel-am <96318827+amel-am@users.noreply.github.com>
2023-11-04 07:08:07 +0330

    Made use of the BASE_API_URL constant and changed the name of an argument that belongs to the request method.

--- src/ocean_client.py
+++ src/ocean_client.py
@@ -1,14 +1,13 @@
 from datetime import datetime
 from aiohttp import ClientSession
 from .yaml import ocean_token
-from .constants import RequestMethods
+from .constants import RequestMethods, BASE_API_URL
 
 
 class DigitalOceanClient(object):
     def __init__(self):
         self._headers = {"Content-Type": 'application/json',
                          "Authorization": f'Bearer {ocean_token}'}
-        self._api_url = "https://api.digitalocean.com/v2"
         self._ratelimit_result = ""
         self._ratelimit_time = None
 
@@ -20,24 +19,24 @@ class DigitalOceanClient(object):
     def limit_time(self):
         return self._ratelimit_time
 
-    async def _request(self, method: str, link: str) -> dict:
+    async def _request(self, method: str, path: str) -> dict:
         async with ClientSession() as session:
-            async with session.request(method=method, url=link, headers=self._headers) as response:
+            async with session.request(method=method, url=f"{BASE_API_URL}/{path}", headers=self._headers) as response:
                 if response.status != 200:
                     return print(f"Something went wrong, status code is: {response.status}")
                 await self._set_ratelimits(response.headers)
                 return await response.json()
 
     async def get_keys(self) -> list:
-        response = await self._request(RequestMethods.GET, f"{self._api_url}/account/keys")
+        response = await self._request(RequestMethods.GET, "account/keys")
         return response["ssh_keys"]
 
     async def get_droplets(self) -> list:
-        response = await self._request(RequestMethods.GET, f"{self._api_url}/droplets")
+        response = await self._request(RequestMethods.GET, "droplets")
         return response["droplets"]
 
     async def get_account(self) -> dict:
-        response = await self._request(RequestMethods.GET, f"{self._api_url}/account")
+        response = await self._request(RequestMethods.GET, "account")
         return response["account"]
 
     async def _set_ratelimits(self, dict_: dict) -> None:
